07a0f6f6b1ed3b397c9417235f73091faca83eca
Tina Shi <init.defaultBranch>
2023-06-05 13:51:33 +1000

    edited functionality

---
+++ config/environment.rb
@@ -0,0 +1,10 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+require 'net/http'
+
+
+require_relative '../lib/news_reader.rb'
+require_relative '../lib/news/cli.rb'
+# require_relative '../lib/brewery/cli'
---
+++ lib/news/cli.rb
@@ -0,0 +1,6 @@
+class NewsReader::CLI
+    def welcome
+        puts 'Welcome to the Inshorts News Reader'
+        puts 'Here is a list of categories to select from:'
+    end
+end
