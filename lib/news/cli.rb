015b6b79962c9eab8af026b947b72c3a0777b4a5
Tina Shi <init.defaultBranch>
2023-06-06 00:31:00 +1000

    get article information from api

--- lib/news/cli.rb
+++ lib/news/cli.rb
@@ -1,22 +1,37 @@
 class NewsReader::CLI
     def welcome
         puts 'Welcome to the News Reader'
-        category_list
-
+        article_list
+        # valid_input
+        read_article
     end
 
-    def category_list
+    def article_list
         puts 'Here is a list of articles to select from:'
-        articles = NewsReader::API.new.articles.uniq
-        # puts categories[1]
+        articles = NewsReader::API.articles.uniq
+        # puts articles[1]
         articles.each.with_index(1) do |item, index|
             if index < 7
                 puts "#{index}. #{item}"
             end
         end
-        puts "Please type the article that most interests you to read more:"
-        num = gets.to_i
+        puts "Please type the number of the article that most interests you to read more:"
 
+    end
+
+    def valid_input
+        num = gets.to_i
+        
+    end
+
+    def read_article
+        article = NewsReader::Article.all
+        # article.each.with_index(1) do |item, index|
+        #     if index < 3
+        #         puts "#{index}. #{article}"
+        #     end
+        # # end
+        # binding.pry
     end
 
     # menu options could instead sort by category?
