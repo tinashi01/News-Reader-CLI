9c51d77ca40b81cb2e88a3e08ac69f97e28f46ae
Tina Shi <init.defaultBranch>
2023-06-05 22:38:56 +1000

    updated article

--- lib/news/cli.rb
+++ lib/news/cli.rb
@@ -1,17 +1,26 @@
 class NewsReader::CLI
     def welcome
-        puts 'Welcome to the News Reader'
-        category_list
-
+        # puts 'Welcome to the Australian News Reader'
+        # category_list
+        puts 'Welcome to the Top Headlines in Australian Technology News Reader'
+        article_list
     end
 
     def category_list
-        puts 'Here is a list of sources to select from:'
-        categories = NewsReader::API.new
-        puts categories.category_type.uniq
-        puts "Please type the category that most interests you:"
+        puts 'Here is a list of categories to select from:'
+        categories = NewsReader::API.new.category_type.uniq
+        categories.each.with_index(1) do |item, index|
+            if index < 5 do
+                puts "#{index}. #{item}"
+            end
+        end
+        puts "Please type the number of the category that most interests you:"
+
     end
 
+    def article_list
+
+    end
     # menu options could instead sort by category?
     # 
     
