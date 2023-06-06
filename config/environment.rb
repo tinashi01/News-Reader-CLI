fd74b08b0b467c19746a5fa2b6fb1b2596cd4b7b
Tina Shi <init.defaultBranch>
2023-06-07 01:01:58 +1000

    update files

--- config/environment.rb
+++ config/environment.rb
@@ -9,6 +9,5 @@ require 'colorize'
 require_relative '../lib/news_reader.rb'
 require_relative '../lib/news/cli.rb'
 require_relative '../lib/news/article.rb'
-require_relative '../lib/news/scraper.rb'
 require_relative '../lib/news/api.rb'
 
